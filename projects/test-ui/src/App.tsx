0aac1413f0b1f043f06067a9811f20ea770ddae4
王玮 <wangwei.1001@bytedance.com>
2024-11-09 14:16:22 +0800

    feat: m

--- projects/test-ui/src/App.tsx
+++ projects/test-ui/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {main} from 'test-core';
+import { main } from 'test-core-ww';
 function App() {
   const [count, setCount] = useState(0)
 
